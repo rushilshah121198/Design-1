1ac6f72d393d1fa8919c65156ac2ce98ca06398a
rushilshah121198 <86422497+rushilshah121198@users.noreply.github.com>
2021-09-14 02:42:29 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :o(1)
+// Space Complexity :o(1)
+// Did this code successfully run on Leetcode : yes this code successfully run on leetcode.
+// Any problem you faced while coding this : this code is not running my laptop. its give me some errors. so can you please solve my error.?
 
 
 // Your code here along with comments explaining your approach
